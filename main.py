f6bdc929fadb665b7cfb0f56d73b00806f93db48
Manav M <manav.maj@outlook.com>
2024-02-19 20:16:31 -0500

    new commit

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask
+import os
+from supabase import create_client, Client
+
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+@app.route('/')
+def default():
+    return "Hello World"
 
-if __name__ == "__main__":
-  app.run()
+
+if __name__ == '__main__':
+    app.run(debug=True)
