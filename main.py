649ade0affd8678013db572805f320cf6b663fd0
Manav M <manav.maj@outlook.com>
2024-02-20 00:20:25 -0500

    added changes

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import os
 from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
 
 from flask import Flask, request
 
 app = Flask(__name__)
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(url, key)
+
+data, count = supabase.table('users').insert({"id":1, "username":"fire"}).execute()
 
 @app.route('/')
 def default():
