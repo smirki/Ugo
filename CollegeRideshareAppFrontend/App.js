fcb8914a38a1c4fb5d2ae4a69f2f3276aa03c53f
Manav M <manav.maj@outlook.com>
2024-02-20 12:00:45 -0500

    added home screen

--- CollegeRideshareAppFrontend/App.js
+++ CollegeRideshareAppFrontend/App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image , Button} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Image source={require('./assets/logo.png')} style={styles.mainCircleImage} />
+      <Text style={styles.bigText} >Welcome to College Rideshare</Text>
+      <Button
+  title="Learn More"
+  color="#841584"
+  accessibilityLabel="Learn more about this purple button"
+/>
     </View>
   );
 }
@@ -13,8 +20,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FECC4C',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bigText: {
+    fontSize: 40,
+    textAlign: "center",
+    color: 'black',
+  },
+  mainCircleImage: {
+    height: 80,
+    width: 80,
+    borderRadius: 40,
+  }
 });
