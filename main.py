bfed111b5ee87c0a58fc249ba94372e7451932c6
Manav M. <kookygavinquack@gmail.com>
2024-02-29 13:53:42 -0500

    added initial driver functions

--- main.py
+++ main.py
@@ -3,8 +3,15 @@ import os
 
 
 from flask import Flask, request, abort
+from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', 
+            static_folder='Tests',)
+socketio = SocketIO(app, cors_allowed_origins='*')
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 app.config['SECRET_KEY'] = 'mysecretkey'
 import Database.db
 
@@ -14,3 +21,4 @@ import Routes.ride_routes
 import Routes.payment_routes
 if __name__ == '__main__':
     app.run(debug=True)
+
